4354526904c721cc841ec8a959a5a4c041dc161d
Traun Leyden <traun.leyden@gmail.com>
2026-06-23 15:26:48 +0200

    bump to version 0.2.1 (#112)

--- crates/core/src/application/first_run_setup.rs
+++ crates/core/src/application/first_run_setup.rs
@@ -11,7 +11,7 @@ use crate::domain::setup::{
 };
 use crate::ports::setup_system::SetupSystemPort;
 
-pub const CURRENT_RELEASE: &str = "v0.1.9";
+pub const CURRENT_RELEASE: &str = "v0.2.1";
 
 pub struct FirstRunSetupUseCase {
     port: Arc<dyn SetupSystemPort>,
