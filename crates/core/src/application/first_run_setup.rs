06b4056c8382096570bb58bb7580a1a2da14be0c
Traun Leyden <traun.leyden@gmail.com>
2026-07-07 18:08:21 +0200

    bump version 0.2.11

--- crates/core/src/application/first_run_setup.rs
+++ crates/core/src/application/first_run_setup.rs
@@ -13,7 +13,7 @@ use crate::domain::setup::{
 };
 use crate::ports::setup_system::SetupSystemPort;
 
-pub const CURRENT_RELEASE: &str = "v0.2.10";
+pub const CURRENT_RELEASE: &str = "v0.2.11";
 
 #[derive(Clone)]
 pub struct FirstRunSetupUseCase {
